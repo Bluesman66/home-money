4bbbd15dfc2738a89ca1d9fd48810c29e3289191
alex.paclin <alex.paclin@gmail.com>
2018-01-22 15:23:14 +0500

    lesson-7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpModule } from '@angular/http';
 import { AuthModule } from './auth/auth.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersService } from './shared/services/users.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { AppComponent } from './app.component';
     AuthModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [UsersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
+import { UsersService } from '../../shared/services/users.service';
+import { User } from './../../shared/models/user.model';
+import { Message } from './../../shared/models/message.model';
+
 @Component({
   selector: 'pai-login',
   templateUrl: './login.component.html',
@@ -9,17 +13,41 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
 
   form: FormGroup;
+  message: Message;
 
-  constructor() { }
+  constructor(private usersService: UsersService) { }
 
   ngOnInit() {
+    this.message = new Message('danger', '');
     this.form = new FormGroup({
       'email': new FormControl(null, [Validators.required, Validators.email]),
       'password': new FormControl(null, [Validators.required, Validators.minLength(6)])
     });
   }
 
+  private showMessage(text: string, type: string = 'danger') {
+    this.message = new Message(type, text);
+    setTimeout(() => {
+      this.message.text = '';
+    }, 5000);
+  }
+
   onSubmit() {
-    console.log(this.form);
-  } 
+    const formData = this.form.value;
+
+    this.usersService.getUserByEmail(formData.email)
+      .subscribe((user: User) => {
+        if (user) {
+          if (user.password === formData.password) {
+            // some logic
+          }
+          else {
+            this.showMessage('Пароль неверный');  
+          }
+        }
+        else {
+          this.showMessage('Такого пользователя не существует');
+        }
+      });
+  }
 }
