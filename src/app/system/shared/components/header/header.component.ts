913fffacdc00a80e2890391ecd2e7e1fb78748d5
alex.paclin <alex.paclin@gmail.com>
2018-01-23 17:50:11 +0500

    lesson-14

---
+++ src/app/system/shared/components/header/header.component.ts
@@ -0,0 +1,30 @@
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+import { User } from '../../../../shared/models/user.model';
+import { AuthService } from './../../../../shared/services/auth.service';
+
+@Component({
+  selector: 'pai-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  date: Date = new Date();
+  user: User;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    this.user = JSON.parse(localStorage.getItem('user'));
+  }
+
+  onLogout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+}
