43d1f2fcf3be560abfbeff69285c14ddbf5784e3
alex.paclin <alex.paclin@gmail.com>
2018-01-26 14:59:25 +0500

    lesson-22

--- src/app/system/records-page/add-event/add-event.component.ts
+++ src/app/system/records-page/add-event/add-event.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Component, OnInit, Input } from '@angular/core';
+import * as moment from 'moment';
+
+import { Category } from './../../shared/models/category.model';
+import { HMEvent } from './../../shared/models/event.model';
 
 @Component({
   selector: 'pai-add-event',
@@ -7,9 +12,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEventComponent implements OnInit {
 
+  @Input() categories: Category[] = [];
+  types = [
+    { type: 'income', label: 'Доход' },
+    { type: 'outcome', label: 'Расход' }
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSubmit(form: NgForm) {    
+    let { type, amount, category, description } = form.value;
+    if (amount < 0) amount *= -1;
+
+    const event = new HMEvent(
+      type, 
+      amount, 
+      category, 
+      moment().format('DD.MM.YYYY HH:mm:ss'), 
+      description);
+  }
 }
