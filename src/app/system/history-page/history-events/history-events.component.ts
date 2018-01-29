c4c78cfc2014759bef19d348628ba0890042e523
alex.paclin <alex.paclin@gmail.com>
2018-01-29 12:39:00 +0500

    lesson-27

--- src/app/system/history-page/history-events/history-events.component.ts
+++ src/app/system/history-page/history-events/history-events.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Category } from './../../shared/models/category.model';
+import { HMEvent } from './../../shared/models/event.model';
 
 @Component({
   selector: 'pai-history-events',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HistoryEventsComponent implements OnInit {
 
+  @Input() categories: Category[] = [];
+  @Input() events: HMEvent[] = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.events.forEach(e => {
+      e.catName = this.categories.find(c => c.id === e.category).name;
+    })
   }
 
+  getEventClass(event: HMEvent) {
+    return {
+      'label': true,
+      'label-danger': event.type === 'outcome',
+      'label-success': event.type === 'income' 
+    }
+  }
 }
--- src/app/system/shared/models/event.model.ts
+++ src/app/system/shared/models/event.model.ts
@@ -1,10 +1,11 @@
 export class HMEvent {
-    constructor(      
+    constructor(
       public type: string,
       public amount: number,
       public category: number,
       public date: string,
       public description: string,
-      public id?: number
+      public id?: number,
+      public catName?: string      
     ) { }
 }
